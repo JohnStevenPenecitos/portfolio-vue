f76a3bae072a016fcb75fea35ff0582cd24f7bdb
JohnStevenPenecitos <steven.penecitos123@gmail.com>
2025-03-05 20:18:03 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/certificates">Certificates</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/Certificates.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>This is for the certificatefsadfdsfsfs fsaf sdfsd fs adsf</div>
+</template>
